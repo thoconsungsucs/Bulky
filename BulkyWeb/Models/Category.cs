84b9328f4c9b8b6df879ab34cd64b9c2b6b2b42d
Thanh <nguyenduythanh181003@gmail.com>
2024-04-23 16:33:21 +0700

    Server side validation Use <span asp-validation-for="<Property>"

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,7 +7,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Category Name")]
+        [MaxLength(20)]
         public String Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be in 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
